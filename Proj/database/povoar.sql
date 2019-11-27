27c0d510200d0169b79a0f928eaa3f7c887eed5b
José Guerra <guerra70@live.com.pt>
2019-11-27 22:09:13 +0000

    elimei pasta proj

--- Proj/database/povoar.sql
+++
@@ -1,3 +0,0 @@
-PRAGMA foreign_keys = ON;
-
-insert into User (Id, Name, DateOfBirth, Email, Username, Password) values (1, 'Pace', '1991-03-03', 'pkloster0@homestead.com', 'pace1234', 'letmein');
--- Proj/templates/tpl_pages.php
+++
@@ -1,5 +0,0 @@
-<?php function draw_homepage()
-{
-    ?>
-
-<?php } ?>
