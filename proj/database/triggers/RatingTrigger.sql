0c8be8304e2dc8c3aa55d6274c23a733467d3b3a
pedrocarmopereira <pppereira3@gmail.com>
2019-12-01 01:17:36 +0000

    Removed triggers dir, triggers should be declares in criar.sql from now on

--- proj/database/triggers/RatingTrigger.sql
+++
@@ -1,9 +0,0 @@
-CREATE TRIGGER updateRating AFTER INSERT ON Review
-
-BEGIN
-
-UPDATE House
-SET Rating = (SELECT SUM(Rating)/COUNT(Rating) FROM (RENT JOIN REVIEW ON RENT.Id = Review.RentID) WHERE Rent.HouseId = (SELECT HOUSE.Id FROM House, Rent WHERE House.Id = (SELECT HouseId FROM Rent WHERE Rent.Id = new.RentId)))
-WHERE Id = (SELECT HOUSE.Id FROM House, Rent WHERE House.Id = (SELECT HouseId FROM Rent WHERE Rent.Id = new.RentId));
-
-END;
